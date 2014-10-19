5f777da14ed752669ca4814c2fc8cbc4bbeca29b
Skyler Berg <skylertheberg@gmail.com>
2014-10-19 16:30:37 -0700

    Add tests for Transform class

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -5,6 +5,40 @@ import mock
 
 import resu
 import resu.transform
+import resu.exceptions
+
+# Subclasses of Transform for testing
+class AddTwo(resu.Transform):
+    '''Assumes data is an integer and adds 2.'''
+    def apply(self, data):
+        return data + 2
+
+class MultiplyByTwo(resu.Transform):
+    '''Assumes data is an integer and adds 2.'''
+    def apply(self, data):
+        return data * 2
+
+class test_get_transforms(unittest.TestCase):
+
+    def test_transforms_present(self):
+        transforms = resu.Transform.get_transforms()
+        assert AddTwo in transforms
+        assert MultiplyByTwo in transforms
+
+class test_composite_transform(unittest.TestCase):
+
+    def test_empty_transforms_list(self):
+        composite = resu.Transform.get_composite_transform([])
+        self.assertEquals(composite, resu.transform._identity)
+
+    def test_unavailable_transform(self):
+        with self.assertRaises(resu.exceptions.TransformNotFoundError):
+            resu.Transform.get_composite_transform(['xyz'])
+
+    def test_compose_two_transforms(self):
+        composite = resu.Transform.get_composite_transform(
+            ['AddTwo', 'MultiplyByTwo'])
+        self.assertEquals(composite(2), 8)
 
 class test_identity(unittest.TestCase):
 
