b4c0c5444b82192bdd4257d65aadb851f4e59811
Skyler Berg <skylertheberg@gmail.com>
2014-11-08 22:01:16 -0800

    Include Parser in main API

--- resu/__init__.py
+++ resu/__init__.py
@@ -3,5 +3,6 @@ from resu.cli import run
 from resu.project import build
 from resu.example import generate_default, get_template
 from resu.transform import Transform
+from resu.parser import Parser
 
 __version__ = '0.0.0'
