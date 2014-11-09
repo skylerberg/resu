f57df8b978f882b72da694e0ba991a9ca3b9c481
Skyler Berg <skylertheberg@gmail.com>
2014-11-08 19:43:32 -0800

    Make type actually tuple for default files

--- resu/defaults.py
+++ resu/defaults.py
@@ -6,4 +6,4 @@ import resu.parsers
 TEMPLATE_ENGINE = resu.template_engines.Jinja2Engine()
 DATA_PARSER = resu.parsers.YamlParser()
 OUTPUT_FILE = 'resu.html'
-DATA_FILES = ('resu.yml')
+DATA_FILES = ('resu.yml', )
