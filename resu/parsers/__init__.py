f19ddec447d377f27dc8b38d4bdfe9222b71e952
Skyler Berg <skylertheberg@gmail.com>
2014-11-09 22:11:13 -0800

    Make modules work without Jinja2, Mako, and PyYAML

--- resu/parsers/__init__.py
+++ resu/parsers/__init__.py
@@ -1,2 +1,5 @@
-from resu.parsers.yaml_parser import YamlParser
+try:
+    from resu.parsers.yaml_parser import YamlParser
+except ImportError:
+    pass
 from resu.parsers.json_parser import JsonParser
--- resu/template_engines/__init__.py
+++ resu/template_engines/__init__.py
@@ -1,2 +1,8 @@
-from resu.template_engines.jinja_engine import Jinja2Engine
-from resu.template_engines.mako_engine import MakoEngine
+try:
+    from resu.template_engines.jinja_engine import Jinja2Engine
+except ImportError:
+    pass
+try:
+    from resu.template_engines.mako_engine import MakoEngine
+except ImportError:
+    pass
