c8f04b065b537efb04386b481d7698236062dde8
Skyler Berg <skylertheberg@gmail.com>
2014-11-13 20:17:32 -0800

    remove references to resu.example module

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,7 +10,6 @@ import resu.cli
 class test_run(unittest.TestCase):
 
     def setUp(self):
-        self.mock_generate_default = mock.patch('resu.example.generate_default').start()
         self.mock_build = mock.patch('resu.build').start()
         self.out = StringIO.StringIO()
 
@@ -18,56 +17,48 @@ class test_run(unittest.TestCase):
         resu.cli.run(args=[], out=self.out)
         output = self.out.getvalue()
         self.assertEquals(output, '')
-        assert not self.mock_generate_default.called
         self.mock_build.assert_called_once()
 
     def test_alternate_data_file(self):
         resu.cli.run(args='resume.yml', out=self.out)
         output = self.out.getvalue()
         self.assertEquals(output, '')
-        assert not self.mock_generate_default.called
         self.mock_build.assert_called_once()
 
     def test_alternate_output_file(self):
         resu.cli.run(args=['-o', 'resu.md'], out=self.out)
         output = self.out.getvalue()
         self.assertEquals(output, '')
-        assert not self.mock_generate_default.called
         self.mock_build.assert_called_once()
 
     def test_alternate_parser(self):
         resu.cli.run(args=['-p', 'json'], out=self.out)
         output = self.out.getvalue()
         self.assertEquals(output, '')
-        assert not self.mock_generate_default.called
         self.mock_build.assert_called_once()
 
     def test_alternate_template(self):
         resu.cli.run(args=['-t', 'fancy'], out=self.out)
         output = self.out.getvalue()
         self.assertEquals(output, '')
-        assert not self.mock_generate_default.called
         self.mock_build.assert_called_once()
 
     def test_help_option(self):
         resu.cli.run(args=['-h'], out=self.out)
         output = self.out.getvalue()
         self.assertEquals(output, resu.cli.CLI_DOC)
-        assert not self.mock_generate_default.called
         assert not self.mock_build.called
 
     def test_version_option(self):
         resu.cli.run(args=['-v'], out=self.out)
         output = self.out.getvalue().strip()
         self.assertEquals(output, resu.__version__)
-        assert not self.mock_generate_default.called
         assert not self.mock_build.called
 
     def test_generate_option(self):
         resu.cli.run(args=['-g'], out=self.out)
         output = self.out.getvalue()
         self.assertEquals(output, '')
-        self.mock_generate_default.assertCalled_once_with()
         assert not self.mock_build.called
 
     def test_invalid_option(self):
@@ -76,7 +67,6 @@ class test_run(unittest.TestCase):
         self.assertNotEquals(cm.exception.code, 0)
         output = self.out.getvalue()
         self.assertEquals(output, resu.cli.CLI_DOC)
-        assert not self.mock_generate_default.called
         assert not self.mock_build.called
 
     def tearDown(self):
