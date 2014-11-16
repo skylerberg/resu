3775a00075fbc7dab163910577b01fd999a047ee
Skyler Berg <skylertheberg@gmail.com>
2014-11-16 14:28:52 -0800

    Add default values for template

--- resu/templates.py
+++ resu/templates.py
@@ -2,27 +2,39 @@ from collections import namedtuple
 
 from resu.sources import PackageDataSource
 
-class Template(namedtuple('Template', [
-        'name',
-        'file_type',
-        'language',
-        'template_source',
-        'example_source'])):
+class Template(
+        namedtuple('Template',
+                   'name template_source example_source file_type language')):
     '''
     :var name: Name of the template. Required.
-    :var file_type: File type the template renders to. Required.
-    :var language: Templating language used in the template. Required.
     :var template_source: Location of the template. Required.
-    :var example_source: Location of example input for the template. Optional.
+    :var example_source: Location of example input for the template.
+      Defaults to ``None``.
+    :var file_type: File type the template renders to.
+      Defaults to ``'html'``.
+    :var language: Templating language used in the template.
+      Defaults to ``'jinja2'``.
     '''
 
     instances = []
 
+    def __new__(cls, 
+                name,
+                template_source,
+                example_source=None,
+                file_type='html',
+                language='jinja2'):
+        # add default values
+        return super(Template, cls).__new__(cls,
+                                            name,
+                                            template_source,
+                                            example_source,
+                                            file_type,
+                                            language)
+
     def __init__(self, **kwargs):
         Template.instances.append(self)
 
-Template(name='default', 
-         file_type='html',
-         language='jinja2',
+Template(name='default',
          template_source=PackageDataSource('resu', 'examples/templates/default.html'),
          example_source=PackageDataSource('resu', 'examples/resu.yml'))
