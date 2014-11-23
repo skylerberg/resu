6e8f3a6ff0060f1cc50e1de289750e724915dd17
Skyler Berg <skylertheberg@gmail.com>
2014-11-22 22:13:04 -0800

    Add module docstring for parers

--- resu/parsers/__init__.py
+++ resu/parsers/__init__.py
@@ -1,3 +1,10 @@
+'''
+Resu expects IO to yield serialized data in need of parsing. To smooth out the
+differences between parsers, Resu provides parsers with a uniform interface.
+For example, PyYAML provides a ``dump`` function while Python's built in json
+module provides a ``dumps`` function. Resu wraps both of these so that other
+functions can call ``dump`` without knowing what parser is being used.
+'''
 from resu.parsers.parser import Parser
 try:
     from resu.parsers.yaml_parser import YamlParser
